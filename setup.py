1124764f878061909ff17667e1252cf213497695
jalan <jalanpalmer@gmail.com>
2013-09-28 13:13:22 -0400

    Allow use under GPL 2 and 3, and clean up setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@
 
 from distutils.core import setup
 
-setup(name = 'filesdir-check',
-      version = '1.1',
-      description = 'filesdir-check helps locate unused FILESDIR files',
-      author = 'Jason Palmer',
-      author_email = 'jalanpalmer@gmail.com',
-      url = 'http://github.com/jalan/filesdir-check',
-      license = 'GPLv2',
-      py_modules = ['filesdir_check'],
-      scripts = ['filesdir-check']
+setup(
+	name='filesdir-check',
+	version='1.1',
+	description='filesdir-check helps locate unused FILESDIR files',
+	author='Jason Alan Palmer',
+	author_email='jalanpalmer@gmail.com',
+	url='http://github.com/jalan/filesdir-check',
+	py_modules=['filesdir_check'],
+	scripts=['filesdir-check'],
+	classifiers=[
+		"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+		]
      )
