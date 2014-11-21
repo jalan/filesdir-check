31daf1feb8b3325b8a909eef689617ae922f4a3c
jalan <jalanpalmer@gmail.com>
2014-11-21 08:59:28 -0500

    pep8 compliance

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
+
 from distutils.core import setup
 
+
 setup(
-	name='filesdir-check',
-	version='1.1',
-	description='filesdir-check helps locate unused FILESDIR files',
-	author='Jason Alan Palmer',
-	author_email='jalanpalmer@gmail.com',
-	url='http://github.com/jalan/filesdir-check',
-	py_modules=['filesdir_check'],
-	scripts=['filesdir-check'],
-	classifiers=[
-		"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
-		]
-	)
+    name='filesdir-check',
+    version='1.1',
+    description='filesdir-check helps locate unused FILESDIR files',
+    author='Jason Alan Palmer',
+    author_email='jalanpalmer@gmail.com',
+    url='http://github.com/jalan/filesdir-check',
+    py_modules=['filesdir_check'],
+    scripts=['filesdir-check'],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        ]
+    )
