42189480e57f5b2442e1653b9997ac6e3910bec1
jalan <jalanpalmer@gmail.com>
2016-12-09 16:20:42 -0500

    simpler license

--- setup.py
+++ setup.py
@@ -1,20 +1,9 @@
-#!/usr/bin/env python
-
-
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
-    name='filesdir-check',
-    version='1.1',
-    description='filesdir-check helps locate unused FILESDIR files',
-    author='Jason Alan Palmer',
-    author_email='jalanpalmer@gmail.com',
-    url='http://github.com/jalan/filesdir-check',
-    py_modules=['filesdir_check'],
-    scripts=['filesdir-check'],
-    classifiers=[
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
-        ]
-    )
+    name="filesdir-check",
+    entry_points={
+        "console_scripts": ["filesdir-check = filesdir_check:main"],
+    },
+)
